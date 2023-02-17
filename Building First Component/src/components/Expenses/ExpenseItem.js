348e4a87f6eba3d41b3b1819595872439ba76502
basantimages <124242939+basantimages@users.noreply.github.com>
2023-02-17 21:07:16 +0530

    States added
    Delete button added to increase amount by 100

--- Building First Component/src/components/Expenses/ExpenseItem.js
+++ Building First Component/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  function clickHandler(e) {
-    const expenses = document.querySelector('.expenses');
-    expenses.removeChild(e.target.parentElement);
-    console.log(e);
-  }
+  // function clickHandler() {}
+  const [amount, setAmount] = useState(props.amount);
+  console.log('ExpenseItem evaluated by React');
+  
+  const clickHandler = () => {
+    setAmount((prev) => prev+100);
+    console.log(amount);
+  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
-      <button onClick={clickHandler} className='expense-item__price'> Delete Expense</button>
+      <button onClick={clickHandler} className='expense-item__price'>Change Amount</button>
     </Card>
   );
 }
