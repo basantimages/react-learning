a377b2c2d7045e521247fb76c0f50863df6be69d
basantimages <124242939+basantimages@users.noreply.github.com>
2023-02-17 15:47:07 +0530

    Styling Components
    Basic stylings added to components and location variable is also added

---
+++ Building First Component/src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure = "Delhi";
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
