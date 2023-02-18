8f8eb007dba1067fd85c9fbe1779f0fbf975ba3f
basantimages <124242939+basantimages@users.noreply.github.com>
2023-02-18 22:14:58 +0530

    Form toggle
    Form toggle feature added so that it opens when user click on Add Expense Button and closes when user click on Close button or submit the expense.

--- Building First Component/src/components/Expenses/Expenses.js
+++ Building First Component/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -12,25 +12,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let myExpenses = <h3>No Expenses to show!!!</h3>;
-
-  let singleExpense = <h3>Only single Expense here. Please add more...</h3>;
-
-  if (filteredExpenses.length > 0) {
-    myExpenses = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-  
   return (
     <div>
       <Card className='expenses'>
@@ -38,8 +23,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 1 && singleExpense}
-        {myExpenses}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
