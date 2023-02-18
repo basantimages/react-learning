f17e866e05b355a8ac467242ee9230397c905ec1
basantimages <124242939+basantimages@users.noreply.github.com>
2023-02-18 16:41:26 +0530

    Expense Form added
    Expense Form added with basic data handling.

--- Building First Component/src/components/Expenses/ExpenseItem.js
+++ Building First Component/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  // function clickHandler() {}
-  const [amount, setAmount] = useState(props.amount);
-  console.log('ExpenseItem evaluated by React');
-  
-  const clickHandler = () => {
-    setAmount((prev) => prev+100);
-    console.log(amount);
-  };
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler} className='expense-item__price'>Change Amount</button>
     </Card>
   );
 }
